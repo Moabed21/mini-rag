504275fb167977fe3c2505beb3c3a0725dfc3272
Moabed21 <moathabed21@gmail.com>
2026-07-03 00:28:44 +0300

    adding HTTP exception in return

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 if you want to design a architecture or a system to a framework, search for a a boilerplate for it
+
+Depends usage: 
+1)  Via FastAPI's Depends() — inside route handlers (correct usage)
+
+2) Via direct call get_settings() — inside controllers 
+
+its better to put constants like hardcoded printing messages in one place 
--- src/routes/data.py
+++ src/routes/data.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends, UploadFile
-
+from fastapi import APIRouter, Depends, UploadFile, status, HTTPException
+from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
 from controllers import DataController
+from models.enums import ResponseSignal
 
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -17,4 +18,13 @@ async def upload_data(project_id : str,file: UploadFile,
     # in controllers then call it here
     is_valid, result = DataController().validate_uploaded_file(file=file)
 
-    return is_valid, result
+    if not is_valid:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=result
+        )
+
+    return JSONResponse(
+        content={"signal": ResponseSignal.FILE_SUCCESSFULLY_VALIDATED.value},
+        status_code=status.HTTP_200_OK
+    )
